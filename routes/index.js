d7689c73c2ac35ce23888e5382a353783e7d0227
Andrew Sheahan <asheahan@gmail.com>
2017-05-04 21:43:35 -0700

    Add image upload feature

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ dest: '/uploads/'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Emote Music' });
+});
+
+router.post('/image/upload', upload.single('file'), function (req, res) {
+  var file = __dirname + '/../uploads/' + req.file.originalname;
+  fs.rename(req.file.path, file, function (err) {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    } else {
+      res.json({
+        message: 'File uploaded successfully',
+        filename: req.file.originalname
+      });
+    }
+  });
 });
 
 module.exports = router;
