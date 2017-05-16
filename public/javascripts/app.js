9f1a4c4ab366332da01e240f0589f83b2f4ad4d8
Andrew Sheahan <asheahan@gmail.com>
2017-05-15 20:54:38 -0700

    Start refactoring to use Vue.js

---
+++ public/javascripts/app.js
@@ -0,0 +1,8 @@
+// app.js
+
+new Vue({
+  el: '#app',
+  data: { }, // register any values or collections that hold data for app
+  ready: function () { }, // anything within ready function will run when application loads
+  methods: { } // methods we want to use in application registered here
+});
