2981a5e982b0193be2b2842480236a8dbbcd64fe
Andrew Sheahan <andrew.sheahan@substantial.com>
2018-12-13 15:22:04 -0800

    Reconfigure happiness recommendations

--- app/api/services/spotify-service.js
+++ app/api/services/spotify-service.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
-const rp = require('request-promise');
+const rp = require('request-promise')
 
 /**
  * authenticate
  * @return {*} token
  */
 exports.authenticate = () => {
-  console.log('authenticate');
+  console.log('authenticate')
 
   let options = {
     uri: 'https://accounts.spotify.com/api/token',
@@ -16,17 +16,18 @@ exports.authenticate = () => {
       Authorization:
         'Basic ' +
         new Buffer(
-          `${process.env.SPOTIFY_CLIENT_ID}:${process.env
-            .SPOTIFY_CLIENT_SECRET}`
+          `${process.env.SPOTIFY_CLIENT_ID}:${
+            process.env.SPOTIFY_CLIENT_SECRET
+          }`,
         ).toString('base64'),
-      Accept: 'application/json'
+      Accept: 'application/json',
     },
     method: 'POST',
-    body: 'grant_type=client_credentials'
-  };
+    body: 'grant_type=client_credentials',
+  }
 
-  return rp(options);
-};
+  return rp(options)
+}
 
 /**
  * getRecommendations
@@ -35,45 +36,43 @@ exports.authenticate = () => {
  * @return {*}
  */
 exports.getRecommendations = (credentials, queryParams) => {
-  console.log('getRecommendations');
+  console.log('getRecommendations')
 
   let options = {
     uri: 'https://api.spotify.com/v1/recommendations',
     qs: calculateParameters(queryParams),
     headers: {
-      Authorization: 'Bearer ' + JSON.parse(credentials).access_token
-    }
-  };
+      Authorization: 'Bearer ' + JSON.parse(credentials).access_token,
+    },
+  }
 
-  return rp(options);
-};
+  return rp(options)
+}
 
 function calculateParameters(params) {
-  let targets = {};
+  let targets = {}
 
   if (params.happiness > 0.5) {
-    targets.target_danceability = params.happiness;
-    targets.target_energy = params.happiness;
-    targets.target_valence = params.happiness;
-    targets.seed_genres = 'happy,party,summer';
+    targets.seed_genres = 'happy,summer'
+    targets.seed_tracks = '6aU6a9tdn2vHhnPGlboFZX'
   }
 
   if (params.sadness > 0.5) {
-    targets.target_valence = 1 - params.sadness;
-    targets.target_danceability = 1 - params.sadness;
-    targets.seed_genres = 'rainy-day,sad';
+    targets.target_valence = 1 - params.sadness
+    targets.target_danceability = 1 - params.sadness
+    targets.seed_genres = 'rainy-day,sad'
   }
 
   if (params.anger > 0.5) {
-    targets.target_valence = 1 - params.anger;
-    targets.target_energy = params.anger;
-    targets.target_loudness = params.anger;
-    targets.seed_genres = 'work-out,hard-rock,metal';
+    targets.target_valence = 1 - params.anger
+    targets.target_energy = params.anger
+    targets.target_loudness = params.anger
+    targets.seed_genres = 'hard-rock,metal'
   }
 
   if (params.surprise > 0.5) {
-    targets.seed_genres = 'funk,party,show-tunes';
+    targets.seed_genres = 'funk,party,show-tunes'
   }
 
-  return targets;
+  return targets
 }
